54ce4893875549b321824139bbe72e5bd27f5e5d
clemarech <poivronrpz@gmail.com>
2018-10-02 12:01:37 +0200

    map1

--- map/ex1.js
+++ map/ex1.js
@@ -22,6 +22,10 @@ En sortie: [
  */
 
 function getStringsLength(strings) {
+let l = strings.map((string) =>{
+  return string + " contains " + string.length + " characters";
+});
+  return l;
 }
 
 // Si besoin, tu peux toujours visualiser le résultat avec console.log,
